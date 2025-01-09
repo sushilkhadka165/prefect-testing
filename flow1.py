c0fed24f14507a684c94a1a2e9bf4f8664dddd88
Sushil Khadka <sushilkhadka165@gmail.com>
2025-01-09 22:09:07 +0545

    initialize

---
+++ flow1.py
@@ -0,0 +1,45 @@
+import random
+from prefect import flow, task
+
+
+@task(log_prints=True)
+def auto_classify_ai():
+    print("Auto classification Started")
+    classified_to_str = ["pdf","excel","none"]
+    classified_to = random.choice(classified_to_str)
+    if classified_to == "pdf":
+        response = pdf_classify()
+    elif classified_to == "excel":
+        response = excel_classify()
+    else:
+        response = "none"
+    return f"return from auto classification {response}"
+
+@task(log_prints=True)
+def excel_classify():
+    print("Excel classification Started")
+    return "return form excel classify"
+
+@task(log_prints=True)
+def pdf_classify():
+    print("Pdf classification Started")
+    return "return form pdf classify"
+
+@task(log_prints=True)
+def upload_files(upload_type):
+    print("uplaod file")
+    if upload_type == "auto_classify_ai":
+        response_ai = auto_classify_ai()
+        print(response_ai)
+    elif upload_type == "excel":
+        response_excel = excel_classify()
+        print(response_excel)
+    elif upload_type == "pdf":
+        response_pdf = pdf_classify()
+        print(response_pdf)
+    return "complete"
+
+
+@flow(log_prints=True)
+def flow1(upload_type):
+    upload_files(upload_type)
